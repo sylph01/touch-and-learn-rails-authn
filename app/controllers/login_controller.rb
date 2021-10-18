5fbad9f8c84e79fdea0fb475d39c1f89e79fb098
Ryo Kajiwara <sylph01@s01.ninja>
2021-10-18 19:55:59 +0900

    fix error message order
    if user is locked out, it should display that it is locked out even 
    before checking password

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,21 +2,21 @@ class LoginController < ApplicationController
   def login
     @user = User.find_by_email(params[:email])
     if @user
-      if @user.password == params[:password]
-        if @user.locked_until && Time.now < @user.locked_until
-          redirect_to root_url, alert: 'User is locked due to too many missed password attempts. Please try again later.'
-        else
+      if @user.locked_until && Time.now < @user.locked_until
+        redirect_to root_url, alert: 'User is locked due to too many missed password attempts. Please try again later.'
+      else
+        if @user.password == params[:password]
           # successful login attempt
           @user.reset_missed_password_attempts
           session[:user] = @user
           cookies[:remember_token] = { value: @user.set_remember_token, expires: 2.days }
           redirect_to protected_url, notice: "Login successful! Hello, #{@user.display_name}!"
+        else
+          # wrong password
+          logger.debug "User is found, wrong password"
+          @user.count_missed_password_attempt
+          redirect_to root_url, alert: 'Email or password is incorrect.'
         end
-      else
-        # wrong password
-        logger.debug "User is found, wrong password"
-        @user.count_missed_password_attempt
-        redirect_to root_url, alert: 'Email or password is incorrect.'
       end
     else
       # user cannot be found
