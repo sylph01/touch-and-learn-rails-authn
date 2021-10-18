46dad810acf79b2117233813f6911664c8556ee5
Ryo Kajiwara <sylph01@s01.ninja>
2021-10-18 18:15:07 +0900

    login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/protected', to: "pages#protected", as: 'protected'
+  post '/login', to: "login#login", as: 'login'
+  post '/logout', to: "login#logout", as: 'logout'
 
   root to: "pages#root"
 end
